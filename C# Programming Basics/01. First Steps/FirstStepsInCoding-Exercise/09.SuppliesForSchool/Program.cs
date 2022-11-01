33183b4eaab7593c90120e98bdb982e5edc76b67
VesKark <vesela.karkova@gmail.com>
2022-11-01 16:42:05 +0100

    Added labs and exercises

---
+++ C# Programming Basics/01. First Steps/FirstStepsInCoding-Exercise/09.SuppliesForSchool/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _09.SuppliesForSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input:
+            int pens = int.Parse(Console.ReadLine()); //number of pen packages
+            int markers = int.Parse(Console.ReadLine()); //number of marker packages
+            int cleaner = int.Parse(Console.ReadLine()); //cleaning material [L]
+            int discount = int.Parse(Console.ReadLine()); //discount [%] from the total sum
+
+            //Calculating costs:
+            double costsPens = pens * 5.80;
+            double costsMarkers = markers * 7.20;
+            double costsCleaner = cleaner * 1.20;
+
+            double totalCosts = costsPens + costsMarkers + costsCleaner;
+            totalCosts = totalCosts - (discount / 100.0) * totalCosts;
+
+            // Output:
+            Console.WriteLine(totalCosts);
+        }
+    }
+}
