d3c4023775be7bdd91121d0eb16bffc41281faa6
VesKark <vesela.karkova@gmail.com>
2022-12-21 20:51:19 +0100

    Added more exercises for Stacks and Queues

---
+++ C# Advanced/01. Stacks and Queues/StacksAndQueues-Exercise/11.KeyRevolver/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {        
+        // input:
+        int bulletPrice = int.Parse(Console.ReadLine());
+        int sizeGunBarrel = int.Parse(Console.ReadLine());
+        int[] bullets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] locks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int intelligencePrice = int.Parse(Console.ReadLine());
+
+        Stack<int> allBullets = new Stack<int>(bullets);
+        Queue<int> allLocks = new Queue<int>(locks);
+
+        // shoot the locks front-to-back, while going through the bullets back-to-front:
+        int barrel = sizeGunBarrel;
+
+        while (allBullets.Count > 0 && allLocks.Count > 0)
+        {
+            if (allBullets.Pop() <= allLocks.Peek())
+            {
+                Console.WriteLine("Bang!");
+                allLocks.Dequeue();
+            }
+            else
+            {
+                Console.WriteLine("Ping!");
+            }
+
+            barrel--;
+
+            if (barrel == 0 && allBullets.Count > 0)
+            {
+                Console.WriteLine("Reloading!");
+                barrel = sizeGunBarrel;
+            }
+        }
+
+        // output:        
+        if (allLocks.Count == 0)
+        {
+            int bulletsTotalPrice = bulletPrice * (bullets.Length - allBullets.Count);
+            int moneyEarned = intelligencePrice - bulletsTotalPrice;
+            Console.WriteLine($"{allBullets.Count} bullets left. Earned ${moneyEarned}");
+        }
+        else if (allBullets.Count == 0)
+        {
+            Console.WriteLine($"Couldn't get through. Locks left: {allLocks.Count}");
+        }
+    }
+}
---
+++ C# Advanced/01. Stacks and Queues/StacksAndQueues-Exercise/12.CupsAndBottles/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // input:
+        int[] cupsCapacity = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] watterBottles = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        Queue<int> allCups = new Queue<int>(cupsCapacity);
+        Stack<int> allBottles = new Stack<int>(watterBottles);
+
+        // fill cups with water:
+        int wastedWater = 0;
+
+        while (allCups.Count > 0 && allBottles.Count > 0)
+        {
+            int bottle = allBottles.Peek();
+            int cup = allCups.Peek();
+
+            if (bottle >= cup)
+            {
+                wastedWater += bottle - cup;
+                allBottles.Pop();
+                allCups.Dequeue();
+            }
+            else
+            {
+                while (cup > 0)
+                {
+                    cup -= bottle;
+                    allBottles.Pop();
+
+                    if (allBottles.Count == 0)
+                    {
+                        break;
+                    }
+
+                    bottle = allBottles.Peek();
+                }
+
+                wastedWater += Math.Abs(cup);
+                allCups.Dequeue();
+            }
+        }
+
+        // output:
+        if (allBottles.Count == 0)
+        {
+            Console.WriteLine("Cups: " + string.Join(" ", allCups));
+        }
+        else
+        {
+            Console.WriteLine("Bottles: " + string.Join(" ", allBottles));
+        }
+
+        Console.WriteLine($"Wasted litters of water: {wastedWater}");
+    }
+}
