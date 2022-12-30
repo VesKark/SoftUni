e833b8d1c7247c366772aef9bb19fb99f1c49a9d
VesKark <vesela.karkova@gmail.com>
2022-12-30 12:23:07 +0100

    Added more exercises for Dictionaries and Sets

---
+++ C# Advanced/03. Sets and Dictionaries Advanced/SetsAndDictionariesAdvanced-Exercise/09.SoftUniExamResults/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<string, int> results = new Dictionary<string, int>();
+        Dictionary<string, int> submissions = new Dictionary<string, int>();
+
+        string input = string.Empty;
+
+        // 1. input exam data:
+        while ((input = Console.ReadLine()) != "exam finished")
+        {
+            string[] participant = input.Split('-');
+            string username = participant[0];
+            string language = participant[1];
+
+            //check if user was banned from exam        
+            if (participant.Length == 2)
+            {
+                if (results.ContainsKey(username))
+                {
+                    results.Remove(username);
+                }
+                continue;
+            }
+            
+            // input user points from exam:
+            int points = int.Parse(participant[2]);
+
+            if (!results.ContainsKey(username))
+            {
+                results[username] = points;
+            }
+            else if (points > results[username])
+            {
+                results[username] = points;
+            }
+
+            if (!submissions.ContainsKey(language))
+            {
+                submissions[language] = 1;
+            }
+            else
+            {
+                submissions[language] += 1;
+            }
+        }
+
+        // 2. print exam results:
+        Console.WriteLine("Results:");
+        PrintExamData(results, '|');
+
+        // 3. print exam submissions:
+        Console.WriteLine("Submissions:");
+        PrintExamData(submissions, '-');
+    }
+
+    public static void PrintExamData(Dictionary<string, int> examData, char separator)
+    {
+        foreach (var user in examData.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+        {
+            Console.WriteLine($"{user.Key} {separator} {user.Value}");
+        }
+    }
+}
---
+++ C# Advanced/03. Sets and Dictionaries Advanced/SetsAndDictionariesAdvanced-Exercise/10.ForceBook/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static Dictionary<string, HashSet<string>> forceBook;
+    static Dictionary<string, string> allUsers;
+    private static void Main(string[] args)
+    {
+        forceBook = new Dictionary<string, HashSet<string>>(); //side -> Set of members
+        allUsers = new Dictionary<string, string>(); //user -> side
+        string input = string.Empty;
+
+        while ((input = Console.ReadLine()) != "Lumpawaroo")
+        {
+            string[] currUser = input.Split(new string[] { " | ", " -> " }, StringSplitOptions.None);
+
+            if (input.Contains(" | "))
+            {
+                string side = currUser[0];
+                string user = currUser[1];
+
+                if (allUsers.ContainsKey(user))
+                {
+                    continue;
+                }
+                
+                AddUser(user, side);
+            }            
+            else if (input.Contains(" -> "))
+            {
+                string user = currUser[0];
+                string side = currUser[1];
+
+                if (allUsers.ContainsKey(user))
+                {
+                    string currSide = allUsers[user];
+                    forceBook[currSide].Remove(user);
+                }
+                
+                AddUser(user, side); //change user side                
+                Console.WriteLine($"{user} joins the {side} side!");
+            }
+        }
+
+        // print forcebook:
+        foreach (var side in forceBook.OrderByDescending(user => user.Value.Count).ThenBy(user => user.Key))
+        {
+            if (side.Value.Count == 0)
+            {
+                break;
+            }
+
+            Console.WriteLine($"Side: {side.Key}, Members: {side.Value.Count}");
+
+            foreach (string user in side.Value.OrderBy(name => name))
+            {
+                Console.WriteLine($"! {user}");
+            }
+        }
+    }
+
+    public static void AddUser(string user, string side)
+    {
+        if (!forceBook.ContainsKey(side))
+        {
+            forceBook[side] = new HashSet<string>();
+        }
+
+        forceBook[side].Add(user);
+        allUsers[user] = side;
+    }
+}
