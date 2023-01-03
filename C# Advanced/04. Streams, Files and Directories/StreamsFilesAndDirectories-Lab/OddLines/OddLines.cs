c1a3715af66740caccfeaf01b0465ccd542006db
VesKark <vesela.karkova@gmail.com>
2023-01-03 17:32:17 +0100

    Added Skeleton for the LabStreamsFilesDirectories

---
+++ C# Advanced/04. Streams, Files and Directories/StreamsFilesAndDirectories-Lab/OddLines/OddLines.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{	
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader inputFile = new StreamReader(inputFilePath))
+            {
+                string line = inputFile.ReadLine();
+                int row = 0;
+
+                using (StreamWriter outputFile = new StreamWriter(outputFilePath))
+                {
+                    while (line != null)
+                    {
+                        if (row % 2 != 0)
+                        {
+                            outputFile.WriteLine(line);
+                        }
+
+                        row++;
+                        line = inputFile.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
